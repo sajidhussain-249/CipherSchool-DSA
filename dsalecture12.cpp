52dde4ab55adfe72947af666cef27c5b5c15900d
sajidhussain-249 <sajidhussain@lpu.in>
2024-07-16 22:26:50 +0530

    Add files via upload

---
+++ dsalecture12.cpp
@@ -0,0 +1,12 @@
+//do while loop 
+#include <iostream>
+using namespace std;
+int main(){
+    int password;
+    
+    do{
+        cout<<"Enter password: ";
+        cin>>password;
+    }while(password!=595959);
+    cout<<"entered password is correct";
+}
